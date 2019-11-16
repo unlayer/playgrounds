42c7f7b330bb19854790b0cf8c5af278b72075a0
Umair Siddique <umairsiddique@gmail.com>
2019-11-16 11:44:55 +0500

    Updated angular basic demo.

--- angular/basic/src/app/app.component.ts
+++ angular/basic/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailEditorComponent } from 'angular-email-editor';
 
 @Component({
   selector: 'app-root',
--- angular/basic/src/app/app.module.ts
+++ angular/basic/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { EmailEditorModule } from 'angular-email-editor';
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    EmailEditorModule,
     BrowserModule
   ],
   providers: [],
