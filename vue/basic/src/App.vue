b89634609a17d66f68c9aed80833b6c6b0a1b368
Umair Siddique <umairsiddique@gmail.com>
2019-11-16 12:40:57 +0500

    Updated vue basic project.

--- vue/basic/src/App.vue
+++ vue/basic/src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <EmailEditor />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import unlayer from 'vue-email-editor'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    EmailEditor: unlayer.EmailEditor
   }
 }
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+#app .container {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 100%;
+}
+#editor {
+  flex: 1;
+  display: flex;
 }
 </style>
